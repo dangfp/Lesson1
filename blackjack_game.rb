c8b88c7c17ac603005cf4ba393f9293023d626c8
dangfp <dangfeipeng@gmail.com>
2014-10-08 16:41:54 +0800

    supplement card 9.

--- blackjack_game.rb
+++ blackjack_game.rb
@@ -52,7 +52,7 @@ end
 puts "Welcome to Blackjack!"
 
 suits = ['H', 'D', 'S', 'C']
-cards = ['2', '3', '4', '5', '6', '7', '8', '10', 'J', 'Q', 'K', 'A']
+cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 deck = suits.product(cards)
 deck.shuffle!
